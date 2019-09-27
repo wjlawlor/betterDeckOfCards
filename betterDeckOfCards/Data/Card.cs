90e35b53cc01f7f4ef6ce2e98ca8c11f3692b178
wjlawlor <wjl28@msn.com>
2019-09-27 11:49:11 -0400

    Almost empty project.

---
+++ betterDeckOfCards/Data/Card.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace betterDeckOfCards.Data
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public bool Drawn { get; set; }
+
+        [Column("CardOrder")]
+        public int Order { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Value { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        public string Suit { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(2)]
+        public string Code { get; set; }
+
+        [Required]
+        public Deck Deck { get; set; }
+        public int DeckId { get; set; }
+
+        public Pile Pile { get; set; }
+        public int? PileId { get; set; }
+    }
+}
